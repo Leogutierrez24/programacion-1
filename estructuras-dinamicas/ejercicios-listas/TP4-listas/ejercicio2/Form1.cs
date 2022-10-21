a989e1885a72c36754dc1bdeba3e2842d39b77d5
Leogutierrez24 <leoalegutierrez97@gmail.com>
2022-10-20 22:04:49 -0300

    in TP4-listas ejercicio02 created the list, u can add new student and print the list

--- estructuras-dinamicas/ejercicios-listas/TP4-listas/ejercicio2/Form1.cs
+++ estructuras-dinamicas/ejercicios-listas/TP4-listas/ejercicio2/Form1.cs
@@ -12,11 +12,11 @@ namespace ejercicio2
 {
     public partial class Form1 : Form
     {
-        Lista listaAlumnos;
+        ListaAlumnos nuevaLista;
         public Form1()
         {
             InitializeComponent();
-            listaAlumnos = new Lista();
+           nuevaLista = new ListaAlumnos();
         }
 
         private void button1_Click(object sender, EventArgs e) // Agregar nuevo alumno (último)
@@ -28,78 +28,110 @@ namespace ejercicio2
                     Nombre = textBox1.Text,
                     Apellido = textBox2.Text,
                     Dni = textBox3.Text,
-                    FechaNac = (dateTimePicker1.Value).ToString(),
-                    Direccion = textBox4.Text,
-                    Telefono = textBox5.Text,
+                    FNacimiento = dateTimePicker1.Value.ToShortDateString(),
+                    Dir = textBox4.Text,
+                    Tel = textBox5.Text
                 };
-                listaAlumnos.agregarAlumno(nuevoAlumno);
-                MessageBox.Show($"Agregaste a {nuevoAlumno.Nombre} {nuevoAlumno.Apellido} como nuevo alumno!!!");
+                nuevaLista.AgregarAlumno(nuevoAlumno);
+                MessageBox.Show($"Se agrego al alumno {nuevoAlumno.Nombre} {nuevoAlumno.Apellido}");
+                clearTextbox();
+            } catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void button6_Click(object sender, EventArgs e) // Imprime la lista 
+        {
+            try
+            {
+                listView1.Items.Clear();
+                Alumno primerAlumno = nuevaLista.primerAlumno();
+                if (primerAlumno == null) MessageBox.Show("No hay ningún alumno para imprimir.");
+                else imprimirLista(primerAlumno);
             } catch (Exception ex)
             {
                 MessageBox.Show(ex.Message);
             }
         }
 
-        private void button6_Click(object sender, EventArgs e)
+        private void clearTextbox()
         {
-
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox5.Text = "";
         }
 
+        private void imprimirLista(Alumno unAlumno)
+        {
+            ListViewItem itemAlumno = new ListViewItem(unAlumno.Nombre);
+            itemAlumno.SubItems.Add(unAlumno.Apellido);
+            itemAlumno.SubItems.Add(unAlumno.Dni);
+            itemAlumno.SubItems.Add(unAlumno.FNacimiento);
+            itemAlumno.SubItems.Add(unAlumno.Dir);
+            itemAlumno.SubItems.Add(unAlumno.Tel);
+            listView1.Items.Add(itemAlumno);
+            if (unAlumno.Siguiente != null) imprimirLista(unAlumno.Siguiente);
+        }
     }
 }
 
-public class Lista
+public class ListaAlumnos
 {
-    Alumno Centinela;
-
-    public Lista()
+    Alumno Puntero;
+    public ListaAlumnos()
     {
-        Centinela = new Alumno();
+        Puntero = new Alumno();
     }
 
-    public void agregarAlumno(Alumno nuevoAlumno)
+    public void AgregarAlumno(Alumno nuevoAlumno)
     {
-        if (Centinela.Siguiente == null) Centinela.Siguiente = nuevoAlumno;
-        else ultimoAlumno(Centinela.Siguiente).Siguiente = nuevoAlumno;
+        if (Puntero.Siguiente == null) Puntero.Siguiente = nuevoAlumno;
+        else ultimoAlumno(Puntero.Siguiente).Siguiente = nuevoAlumno;
     }
 
-    private Alumno ultimoAlumno(Alumno unAlumno)
+    public Alumno ultimoAlumno(Alumno unAlumno)
     {
-        Alumno auxAlumno = unAlumno;
-        if (unAlumno.Siguiente != null) auxAlumno = ultimoAlumno(unAlumno.Siguiente);
-        return auxAlumno;
+        Alumno elUltimo = unAlumno;
+        if (unAlumno.Siguiente != null) ultimoAlumno(unAlumno.Siguiente);
+        return elUltimo;
     }
 
-    public int cantidadAlumnos(Alumno unAlumno, int cantidad)
+    public Alumno primerAlumno()
     {
-        int nAlumnos = 5;
-        
-        return nAlumnos;
+        Alumno elPrimero = null;
+        if (Puntero.Siguiente == null) return elPrimero;
+        else
+        {
+            elPrimero = Puntero.Siguiente;
+            return elPrimero;
+        }
     }
-
 }
 
 public class Alumno
 {
-    public Alumno(string nombre = "", string apellido = "", string dni = "", string fechaNac = "", string direccion = "", string telefono = "")
+    public Alumno(string nombre = "", string apellido = "", string dni = "", string fNacimiento = "", string dir = "", string tel = "")
     {
         Nombre = nombre;
         Apellido = apellido;
         Dni = dni;
-        FechaNac = fechaNac;
-        Direccion = direccion;
-        Telefono = telefono;
-        Siguiente = null;
+        FNacimiento = fNacimiento;
+        Dir = dir;
+        Tel = tel;
         Anterior = null;
+        Siguiente = null;
     }
 
     public string Nombre { get; set; }
     public string Apellido { get; set; }
     public string Dni { get; set; }
-    public string FechaNac { get; set; }
-    public string Direccion { get; set; }
-    public string Telefono { get; set; }
-    public Alumno Siguiente { get; set; }
+    public string FNacimiento { get; set; }
+    public string Dir { get; set; }
+    public string Tel { get; set; }
     public Alumno Anterior { get; set; }
-
+    public Alumno Siguiente { get; set; }
 }

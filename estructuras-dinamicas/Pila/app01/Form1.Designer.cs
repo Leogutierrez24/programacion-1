8cd5f23e6bb9d15cc842a6c16ae16c3aa4365222
Leogutierrez24 <leoalegutierrez97@gmail.com>
2022-12-06 22:48:40 -0300

    started with the stacks

---
+++ estructuras-dinamicas/Pila/app01/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace app01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 25);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(121, 195);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Lista de contenedores";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(152, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 43);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Apilar contenedor";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(152, 74);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(93, 43);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Desapilar contenedor";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(264, 231);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Almacen";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ estructuras-dinamicas/Pila/app01/Pila.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace app01
+{
+    public class Pila
+    {
+        Contenedor Puntero;
+
+        public Pila()
+        {
+            Puntero = new Contenedor();
+        }
+
+        public void push(Contenedor newContenedor)
+        {
+            if (Puntero.Siguiente == null) Puntero.Siguiente = newContenedor;
+            else
+            {
+                Contenedor lastContenedor = Puntero.Siguiente;
+                newContenedor.Siguiente = lastContenedor;
+                Puntero.Siguiente = newContenedor;
+            }
+        }
+
+        public Contenedor pop()
+        {
+            Contenedor contenedorAux = Puntero.Siguiente;
+            if (contenedorAux != null)
+            {
+                Puntero.Siguiente = contenedorAux.Siguiente;
+                contenedorAux.Siguiente = null;
+            }
+
+            return contenedorAux;
+        }
+
+        public Contenedor getLastOne()
+        {
+            Contenedor auxContenedor = Puntero.Siguiente;
+            return auxContenedor;
+        }
+
+        private Contenedor findContenedorById(Contenedor unContenedor, string id)
+        {
+            Contenedor auxContenedor = Puntero.Siguiente;
+            if(auxContenedor != null)
+            {
+                if (auxContenedor.Id != id) auxContenedor = findContenedorById(auxContenedor.Siguiente, id);
+            }
+            return auxContenedor;
+        }
+
+
+    }
+}

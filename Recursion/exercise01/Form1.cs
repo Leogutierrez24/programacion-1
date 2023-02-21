92aea9fdade2f9d201b9a95525a1040c03e41207
Leogutierrez24 <leoalegutierrez97@gmail.com>
2023-02-21 18:22:05 -0300

    started with new exercises about recursive functions

---
+++ Recursion/exercise01/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/* Escribir una función recursiva que devuelva la suma de los primeros N enteros. */
+
+namespace exercise01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int GetOperation(int maxNumber)
+        {
+            int result = 0;
+            if (maxNumber != 0) result += maxNumber + GetOperation(maxNumber - 1);
+            return result;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int maxNumber = (int)numericUpDown1.Value;
+                int result = GetOperation(maxNumber);
+
+                label1.Text = result.ToString();
+            } catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
---
+++ Recursion/exercise02/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/* Escribir una función recursiva que devuelva los primeros N números pares. */
+
+namespace exercise02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string GetEvenNumbers(int maxNumber, int i, string result)
+        {
+            if (i != maxNumber + 1)
+            {
+                if (i % 2 == 0) result += i + " " + GetEvenNumbers(maxNumber, i + 1, result);
+                else result += GetEvenNumbers(maxNumber, i + 1, result);
+            }
+            return result;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int maxNumber = (int)numericUpDown1.Value;
+                label1.Text = GetEvenNumbers(maxNumber, 1, "");
+            } catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
